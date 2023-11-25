d9139aaeff5c7cb31648275ea1173829ed12ac28
Anmol Khurana <123490433+anmolkhurana490@users.noreply.github.com>
2023-11-25 21:01:06 +0530

    Update index.py

--- index.py
+++ index.py
@@ -9,7 +9,7 @@ blockchain_manager = BlockchainManager()
 
 @app.route("/")
 def index():
-		return render_template('index.html')
+	return render_template('index.html')
 
 @app.route("/home", methods=["GET", "POST"])
 def home():
