4a6ec0dc5f9a32ecfbf4ceca20f738f3e13e0005
Anmol Khurana <123490433+anmolkhurana490@users.noreply.github.com>
2023-12-04 18:42:41 +0530

    Add files via upload

--- myapp.py
+++ myapp.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 from blockchain_layer.blockchain_layer import Blockchain
+from ml_layer import ml_model
+from qr_code import qrcode
 import json
 
 app = Flask(__name__)
 app.secret_key = "alkdjfalkdjf"
-#fake_product_analyzer = FakeProductAnalyzer()
 blockchain = Blockchain()
 
 
@@ -29,6 +30,8 @@ def set_user_type(user_type):
         return redirect(url_for("login"))
     elif user_type == "customer":
         return redirect(url_for("search_product"))
+    else:
+        return redirect(url_for("index"))
 
 
 @app.route("/")
@@ -38,7 +41,7 @@ def index():
 
 @app.route("/home", methods=["GET", "POST"])
 def home():
-    if "user" in session:
+    if "user_auth" in session:
         if session["user_type"] == "admin":
             return redirect(url_for('admin'))
         elif session["user_type"] == "manufacturer":
@@ -47,8 +50,10 @@ def home():
             return redirect(url_for('supplier'))
         else:
             session["user_type"] = ""
+            session["user_auth"] = {}
             return redirect(url_for('index'))
     else:
+        session["user_type"] = ""
         flash("Please login to access the home page.")
         return redirect(url_for('index'))
 
@@ -69,7 +74,7 @@ def login():
             elif session["user_type"] == "supplier":
                 return redirect(url_for('supplier'))
             else:
-                session["user_type"]=""
+                session["user_type"] = ""
                 session.pop("user_auth", None)
                 return redirect(url_for('index'))
         else:
@@ -79,67 +84,20 @@ def login():
         return render_template('login.html')
 
 
-@app.route("/verify/<product_id>", methods=["GET"])
-def verify(product_id):
-    return render_template('verify.html', keyId=product_id)
-
-
-@app.route("/verify", methods=["POST"])
-def success():
-
-	post_data = request.form["keyId"]
-
-	with open('./NODES/N1/blockchain.json', 'r') as bfile:
-		n1_data = str(bfile.read())
-	with open('./NODES/N2/blockchain.json', 'r') as bfile:
-		n2_data = str(bfile.read())
-	with open('./NODES/N3/blockchain.json', 'r') as bfile:
-		n3_data = str(bfile.read())
-	with open('./NODES/N4/blockchain.json', 'r') as bfile:
-		n4_data = str(bfile.read())
-
-	pd = str(post_data)
-
-	if (pd in n1_data) and (pd in n2_data) and (pd in n3_data) and (pd in n4_data):
-
-		with open('./NODES/N1/blockchain.json', 'r') as bfile:
-			for x in bfile:
-				if pd in x:
-					a = json.loads(x)["data"]
-					b = a.replace("'", "\"")
-					data = json.loads(b)
-
-					product_brand = data["Manufacturer"]
-					product_name = data["ProductName"]
-					product_batch = data["ProductBatch"]
-					manuf_date = data["ProductManufacturedDate"]
-					expiry_date = data["ProductExpiryDate"]
-					product_id = data["ProductId"]
-					product_price = data["ProductPrice"]
-					product_size = data["ProductSize"]
-					product_type = data["ProductType"]
-
-		return render_template('success.html', brand=product_brand, name=product_name, batch=product_batch, manfdate=manuf_date, exprydate=expiry_date, id=product_id, price=product_price, size=product_size, type=product_type)
-
-	else:
-		return render_template('fraud.html')
-
-
 @app.route("/add_product", methods=["POST", "GET"])
 def add_product():
     if request.method == "POST":
-        session["product_details"] = {
-            "name": request.form["productname"],
-            "brand": request.form["brand"],
-            "product_id": request.form["productid"],
-            "retail_price": request.form["retailprice"],
-            "discounted_price": request.form["discountedprice"],
-            "unique_id": blockchain.unique_id
-        }
+        product_details = {}
+        for key in ["productname", "brand", "productid", "retailprice", "discountedprice"]:
+            product_details[key] = request.form[key]
+        product_details["uniqueid"] = str(blockchain.unique_id)
+        session["product_details"] = product_details
         return redirect(url_for('confirm'))
-    else:
+    
+    elif session["user_type"] and session["user_auth"]:
         return render_template('add_product.html')
-
+    else:
+       return redirect(url_for('index'))
 
 @app.route("/confirm", methods=["GET", "POST"])
 def confirm():
@@ -147,101 +105,111 @@ def confirm():
     if request.method == "POST":
         if product_details:
             blockchain.addproduct(product_details, session["user_auth"])
+            path = qrcode.generate_qr_code(product_details)
             session.pop("product_details", None)  # Clear product_details from the session
             flash("The Product has been added in the Blockchain")
-            return render_template('confirm.html', product_details=product_details, message="The Product has been added in the Blockchain")
+            return render_template('confirm.html', product_details=product_details, message="The Product has been added in the Blockchain", path=path)
 
-    return render_template('confirm.html', product_details=product_details)
+    elif session["user_type"] and session["user_auth"]:
+        return render_template('confirm.html', product_details=product_details)
+    else:
+       return redirect(url_for('index'))
 
 
 @app.route("/admin")
 def admin():
-	if session["user_type"] == "admin":
-		return render_template('add_product.html')
-	else:
-		return redirect(url_for('home'))
+    if session["user_type"] == "admin":
+    	return render_template('add_product.html')
+    else:
+    	return redirect(url_for('home'))
 
 @app.route("/manufacturer")
 def manufacturer():
-	if session["user_type"] == "manufacturer":
-		return render_template('manufacturer.html')
-	else:
-		return redirect(url_for('home'))
+    if session["user_type"] == "manufacturer":
+    	return render_template('manufacturer.html')
+    else:
+    	return redirect(url_for('home'))
 
 @app.route("/supplier")
 def supplier():
-	if session["user_type"] == "supplier":
-		return render_template('supplier.html')
-	else:
-		return redirect(url_for('home'))
+    if session["user_type"] == "supplier":
+    	return render_template('supplier.html')
+    else:
+    	return redirect(url_for('home'))
 
 @app.route("/added")
 def added():
 	return render_template('added.html')
 
 
-@app.route("/verifyNodes")
-def verifyNodes():
-	bc = BlockChain()
-	isBV = bc.isBlockchainValid()
-
-	if isBV:
-		flash("All Nodes of Blockchain are valid")
-		return redirect(url_for('admin'))
-	else:
-		flash("Blockchain Nodes are not valid")
-		return redirect(url_for('admin'))
-
-
 @app.route("/search_product", methods=["GET", "POST"])
 def search_product():
     if request.method == "POST":
-        product_details = {
-            "brand": request.form["brand"],
-            "name": request.form["productname"],
-            "productid": request.form["productid"],
-            "uniqueid": request.form["uniqueid"],
-            "retailprice": request.form["retailprice"],
-            "discountedprice": request.form["discountedprice"],
-        }
-
-        # Analyze product with ML
-        is_authentic_ml = fake_product_analyzer.analyze_product(product_details)
-
-        # Verify product with blockchain
-        product_details = blockchain.getProduct(product_details("uniqueid"))
-        is_on_blockchain = blockchain.verifyProduct(product_details("uniqueid"))
+        qr_code_image = request.files.get('qrCodeImage')
         
+        product_details = {}
+        for key in ["productname", "brand", "productid", "uniqueid", "retailprice", "discountedprice"]:
+            product_details[key] = request.form[key]
+        
+        if qr_code_image:
+            product_details_qr = qrcode.decode_qr_code(qr_code_image)
+            product_details_qr = product_details_qr.replace("'", '"')
+            product_details = json.loads(product_details_qr)
+        
+        elif any(field == "" for field in product_details.values()):
+            flash("Fill all product details!")
+            return render_template('search_product.html', error_message="Please either fill all Product Details or Upload QR Code!")
+        
+        
+        for key in ["productid", "uniqueid", "retailprice", "discountedprice"]:
+            product_details[key] = int(product_details[key])
+        
+        # Verify product with blockchain
+        blockchain_product_details = blockchain.getProduct(product_details["uniqueid"])
+        is_on_blockchain = blockchain_product_details.get("isAuthentic", False)
+        
+        # Analyze product with ML
+        is_authentic_ml = ml_model.productAnalyzer(product_details)
+       
         if is_on_blockchain and is_authentic_ml:
-            isAuthentic = True
+            if all( blockchain_product_details[key] == product_details[key] 
+                 for key in ["productname", "brand", "productid", "retailprice", "discountedprice"] ):
+                isAuthentic = True
+            else:
+                flash("Invalid Product Details!")
+                return render_template('search_product.html', error_message="Invalid Product Details! Enter Again")
         else:
             isAuthentic = False
-
+        
         session["search_result"] = {
-            "product_details": product_details,
+            "product_details": blockchain_product_details,
             "isAuthentic": isAuthentic
         }
 
         return redirect(url_for('search_result'))
 
-    return render_template('search_product.html')
-
+    else:
+        return render_template('search_product.html')
 
 @app.route("/search_result")
 def search_result():
-    search_result = session.get("search_result")
-    session.pop("search_result", None)
-    return render_template('search_result.html', search_result=search_result)
+    if session["search_result"]:
+        search_result = session.get("search_result")
+        session.pop("search_result", None)
+        return render_template('search_result.html', search_result=search_result)
+    else:
+        return redirect(url_for('search_product'))
+    
 
 
 @app.route("/logout")
 def logout():
-    session["user"] = ""
-    session["user_type"] = ""
+    session["user_auth"] = {}
     flash("You have been logged out.")
-    return redirect(url_for('login'))
+    return render_template('login.html', error_message="You have been logged out")
 
 
 if __name__ == "__main__":
     app.run(debug=True)
-    session["user"] = ""
+    session["user_type"] = ""
+    session["user_auth"] = {}
